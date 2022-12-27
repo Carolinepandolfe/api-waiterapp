3c4838949fbba6e67018cd27e1855b7cb37b787b
carolpandolfe <carol@MacBook-Pro-de-Caroline.local>
2022-12-27 18:26:40 -0300

    List and create categories

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { router } from './router';
 
-
-mongoose.connect('mongodb://localhost:27017').then(() =>{
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017').then(() => {
+  console.log('Connected to MongoDB');
   const app = express();
-
   const port = 3001;
+
+  app.use(express.json());
+  app.use(router);
+
   app.listen(port, () => {
     console.log(`🤟🏻 Server is running on http://localhost:${port}`);
   });
